f5bb54bdd0b66d8dc930045f18187d73418c5813
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2020-05-27 00:43:38 +0500

    Final Commit

--- ASCII.py
+++ ASCII.py
@@ -1,5 +1,5 @@
 # Taking input from the user.
 a = input("Enter an alphabet: ")
 
-# Printing the statement with the conversion of an alphabet to the ASCII Code.
-print("The ASCII value of '" + a + "' is", ord(a))
+# Printing the statement and also converting of an alphabet to the ASCII Code.
+print("The ASCII value of '" + a + "' is", ord(a)) 
